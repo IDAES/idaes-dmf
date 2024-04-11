1ad706f69afbf0b43504a56c8ead55178e3332db
Ludovico Bianchi <lbianchi@lbl.gov>
2024-04-11 08:59:26 -0500

    Make package installable

--- src/idaes_dmf/__init__.py
+++ src/idaes_dmf/__init__.py
@@ -19,9 +19,7 @@ to your models.
 __author__ = "Dan Gunter"
 
 import logging
-
-# DMF version is the same as IDAES version
-from idaes import __version__  # noqa
+from importlib.metadata import version
 
 from .dmfbase import DMF, DMFConfig  # noqa: F401
 from .dmfbase import create_configuration  # noqa: F401
@@ -30,10 +28,4 @@ from .userapi import get_workspace  # noqa: F401
 from . import resource  # noqa: F401
 
 
-# default log format
-h = logging.StreamHandler()
-h.setFormatter(
-    logging.Formatter("%(asctime)s [%(levelname)s] " "%(name)s: %(message)s")
-)
-logging.getLogger("idaes.core.dmf").addHandler(h)
-logging.getLogger("idaes.core.dmf").propagate = False
+__version__ = version("idaes-dmf")
